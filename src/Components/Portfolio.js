e14cd4ffac9fa9fb5deaf5590ecd37a5e07de73c
ting682 <tchung682@gmail.com>
2020-11-19 15:47:14 -0500

    added projects

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -5,7 +5,7 @@ class Portfolio extends Component {
 
     if(this.props.data){
       var projects = this.props.data.projects.map(function(projects){
-        var projectImage = 'images/portfolio/'+projects.image;
+        var projectImage = projects.image;
         return <div key={projects.title} className="columns portfolio-item">
            <div className="item-wrap">
             <a href={projects.url} title={projects.title}>
@@ -13,12 +13,14 @@ class Portfolio extends Component {
                <div className="overlay">
                   <div className="portfolio-item-meta">
                  <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
+                     
                   </div>
                 </div>
               <div className="link-icon"><i className="fa fa-link"></i></div>
             </a>
+            
           </div>
+          <p>{projects.category}</p>
         </div>
       })
     }
@@ -30,7 +32,7 @@ class Portfolio extends Component {
 
          <div className="twelve columns collapsed">
 
-            <h1>Check Out Some of My Works.</h1>
+            <h1>Here are some of my projects:</h1>
 
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
                 {projects}
