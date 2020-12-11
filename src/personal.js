cbc14d433418c73dfe0496c5c64e3b01cfed4c7b
ting682 <tchung682@gmail.com>
2020-12-11 10:33:27 -0500

    updated resume

---
+++ src/personal.js
@@ -0,0 +1,9 @@
+
+    let i = {};
+    i.am = 'a full-stack web developer';
+    i.usedTo = ['engineer aircraft systems', 'design hardware', 'manage Chinese aircrafts'];
+    i.love = ['God', 'family', 'friends', 'web application design', 'old movies'];
+    i.workWith = ['ReactJS', 'Ruby on Rails', 'perseverance', 'PostgreSQL'];
+    i.desire = 'to be better version of me every day';
+    i.aspire = 'to make the world a better place through engineering';
+
